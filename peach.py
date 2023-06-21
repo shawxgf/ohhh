b25959048d448d4ee2454f9eab624dedca9b4e81
shawxgf <xgf05191995@gmail.com>
2023-06-21 14:44:54 +0800

    amazon whisper
    using amazom whisper to create a function named readCsv()

--- peach.py
+++ peach.py
@@ -1,2 +1,11 @@
 def addNum(a, b):
     return a+b
+
+# 写一个函数读取csv文件 一行一行返回一个list
+
+def readCsv(path): 
+    with open(path, 'r') as f:
+        lines = f.readlines()
+        return lines
+
+        
